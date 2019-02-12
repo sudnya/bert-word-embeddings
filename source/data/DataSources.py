b4579e695410517022ca05c6acc9e0ebb9365dff
gdiamos <solusstultus@gmail.com>
2019-02-12 08:57:49 -0800

    ADD: Placeholder files for dataset creation.

---
+++ source/data/DataSources.py
@@ -0,0 +1,25 @@
+
+
+from data.AdaptorFactory import AdaptorFactory
+
+class DataSources:
+    def __init__(self, config):
+        self.currentSource = 0
+        self.sources = []
+        self.config = config
+
+    def next(self):
+        item = self.sources[self.currentSource].next()
+
+        self.currentSource = (self.currentSource + 1) % len(self.sources)
+
+        return item
+
+    def addSource(self, source):
+        adaptor = AdaptorFactory(self.config).create(source)
+
+        self.sources[len(self.sources)] = adaptor
+
+
+
+
